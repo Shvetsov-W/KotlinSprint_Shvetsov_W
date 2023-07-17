8bf1888526b12d339491dd7beefd634078f05d36
victor <v89617755598@yandex.ru>
2023-07-17 15:54:53 +0500

    lesson_1_task_5 converted seconds into minutes and hours, created methods for this

---
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -0,0 +1,24 @@
+package lesson_1
+
+import java.sql.Time
+import java.time.LocalDateTime
+
+fun main() {
+    val numberOfSeconds: Int = 6480
+    val (minute: Int, remainderSecond: Int) = secondToMinute(6480)
+    val (hour: Int, remainderMinute: Int) = minuteToHour(minute)
+    println("Время, проведенное в космосе: ${hour.likeDataTime()}:${remainderMinute.likeDataTime()}:${remainderSecond.likeDataTime()}")
+}
+
+fun secondToMinute(second: Int) = ReturnedValue(second / 60, second % 60)
+
+fun minuteToHour(minute: Int) = ReturnedValue(minute / 60, minute % 60)
+
+fun Int.likeDataTime(): String{
+    if (this <= 9) return "0$this"
+    else return "$this"
+}
+
+data class ReturnedValue(val value: Int, val remainder: Int) {
+
+}
