1d98fa469bab5f6983e48e9ffb369bb9dd55556a
victor <v89617755598@yandex.ru>
2023-07-18 20:00:56 +0500

    lesson_1_task_5 simplified and changed the output format

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,21 +1,11 @@
 package lesson_1
 
 fun main() {
-    val numberOfSeconds: Int = 6480
-    val (minute: Int, remainderSecond: Int) = secondToMinute(6480)
-    val (hour: Int, remainderMinute: Int) = minuteToHour(minute)
-    println("Время, проведенное в космосе: ${hour.likeDataTime()}:${remainderMinute.likeDataTime()}:${remainderSecond.likeDataTime()}")
-}
+    val totalSeconds: Int = 6480
+    val hours: Int = totalSeconds / 3600
+    val minutes: Int = (totalSeconds % 3600) / 60
+    val seconds: Int = totalSeconds % 60
 
-fun secondToMinute(second: Int) = ReturnedValue(second / 60, second % 60)
-
-fun minuteToHour(minute: Int) = ReturnedValue(minute / 60, minute % 60)
-
-fun Int.likeDataTime(): String{
-    if (this <= 9) return "0$this"
-    else return "$this"
-}
-
-data class ReturnedValue(val value: Int, val remainder: Int) {
+    println(String.format("Время проведенное в космосе: %02d:%02d:%02d ", hours, minutes, seconds))
 
 }
