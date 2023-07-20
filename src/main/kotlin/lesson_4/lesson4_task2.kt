401811ac6d5ecf9935acc6784fa3eb86fdbba9b4
victor <v89617755598@yandex.ru>
2023-07-20 19:45:43 +0500

    lesson_4_task_2  created a method that checks the cargo parameters

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,2 +1,15 @@
 package lesson_4
+fun main() {
+    val average: Average = Average()
+    val cargo1: Cargo = Cargo(20, 80)
+    val cargo2: Cargo = Cargo(50, 100)
+
+    println(("Груз с весом ${cargo1.weight} кг и объемом ${cargo1.volume} л соответствует категории 'Average':" +
+            "${(cargo1.weight >= average.minWeight || cargo1.weight <= average.maxWeight) && cargo1.volume < average.maxVolume}."))
+    println(("Груз с весом ${cargo2.weight} кг и объемом ${cargo2.volume} л соответствует категории 'Average':" +
+            "${(cargo2.weight >= average.minWeight || cargo2.weight <= average.maxWeight) && cargo2.volume < average.maxVolume}."))
+}
+data class Average(val minWeight: Int = 35, val maxWeight: Int = 100, val maxVolume: Int = 100)
+
+data class Cargo(val weight: Int, val volume: Int)
 
