1c91a46db7a0297feedec2dadafd6218f1f03c73
victor <v89617755598@yandex.ru>
2023-07-20 14:44:13 +0500

    lesson_2_task_5 renamed the function + deleted return

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -7,11 +7,10 @@ fun main() {
     val initialSum: Int = 70_000
     val interestRate: Double = 16.7
     val years: Int = 20
-    val result: Double = compoundInterest(initialSum,  interestRate, 20)
+    val result: Double = calculateInterest(initialSum,  interestRate, 20)
     println(String.format(Locale.ROOT,"Размер вклада через 20 лет: %.3f", result))
 
 }
 
-fun compoundInterest(initialSum: Int, percent: Double, years: Int): Double {
-    return initialSum * (1 + percent / 100).pow(years)
-}
+fun calculateInterest(initialSum: Int, percent: Double, years: Int): Double =
+    initialSum * (1 + percent / 100).pow(years)
