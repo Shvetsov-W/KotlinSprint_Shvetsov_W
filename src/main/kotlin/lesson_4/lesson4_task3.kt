c67735636ecd07716f1458db9f1cb9950dbe49d7
victor <v89617755598@yandex.ru>
2023-07-20 20:12:18 +0500

    lesson_4_task_3  created a method that checks conditions for plant growth

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,2 +1,10 @@
 package lesson_4
 
+fun main() {
+    val isSunny: Boolean = true
+    val isOpenTent: Boolean = true
+    val airHumidity: Int = 20
+    val season: String = "зима"
+
+    println("Благоприятные ли условия сейчас для роста бобовых? ${isSunny && isOpenTent && airHumidity == 20 && season != "зима"}")
+}
