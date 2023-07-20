22241eaa14d02a21ed821a7aa111fbc0ecf6660b
victor <v89617755598@yandex.ru>
2023-07-20 19:16:32 +0500

    lesson_4_task_1 created a method for checking the reservation

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,2 +1,13 @@
 package lesson_4
 
+fun main() {
+    var reservedToday: Int = 13
+    var reservedTomorrow: Int = 9
+
+    checkReserve(reservedToday, reservedTomorrow)
+}
+
+fun checkReserve(reservedToday: Int, reservedTomorrow: Int, numberOfTables: Int = 13) {
+    println("[Доступность столиков на сегодня:${reservedToday < numberOfTables}]")
+    println("[Доступность столиков на завтра:${reservedTomorrow < numberOfTables}]")
+}
