bcdfb9a8169d2d0246fa885503b215e6e39908e1
victor <v89617755598@yandex.ru>
2023-07-20 15:51:57 +0500

    lesson_3_task_3 reformat code

--- src/main/kotlin/lesson_3/lesson3_task3.kt
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -1,9 +1,10 @@
 package lesson_3
 
 fun main() {
-    val number:Int = 5
+    val number: Int = 5
 
-    println("""$number*1=${number * 1}
+    println(
+        """$number*1=${number * 1}
         |$number*2=${number * 2}
         |$number*3=${number * 3}
         |$number*4=${number * 4}
@@ -13,5 +14,6 @@ fun main() {
         |$number*8=${number * 8}
         |$number*9=${number * 9}
         |$number*10=${number * 10}
-    """.trimMargin())
+    """.trimMargin()
+    )
 }
